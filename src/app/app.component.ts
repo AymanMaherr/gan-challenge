8b158df711d6f8042618e8dc4832d88461054b3c
AymanMaherr <ayman.elserogy@gmail.com>
2024-12-10 06:20:35 +0100

    Gan - app component including routes and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CardComponent } from '../components/card/card.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardComponent],
-  template: `
-    <p>hello</p>
-    <app-card/>
-    <router-outlet/>
-  `,
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'gan-challange';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+// import { CdDsipalyPageComponent } from '../pages/cd-dsipaly-page/cd-dsipaly-page.component';
+import { CartPageComponent } from '../pages/cart-page/cart-page.component';
+import { CdDsipalyPageComponent } from '../pages/cd-dsipaly-page/cd-dsipaly-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: CdDsipalyPageComponent,
+  },
+  {
+    path: 'cart',
+    component: CartPageComponent,
+  }
+];
